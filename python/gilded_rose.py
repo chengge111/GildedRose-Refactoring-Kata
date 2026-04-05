43bc9d9f43fe6414c097f7d702fea35a9e13b4b8
ChenggeWen <144627949+ChenggeWen@users.noreply.github.com>
2026-04-05 16:19:35 -0700

    Lab 5: add AI prompt files and refactor gilded rose

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -15,79 +15,70 @@ class Item:
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
-class GildedRose(object):
+class ItemUpdater:
+    def update(self, item):
+        raise NotImplementedError
 
+
+class NormalUpdater(ItemUpdater):
+    def update(self, item):
+        if item.quality > 0:
+            item.quality -= 1
+
+        item.sell_in -= 1
+
+        if item.sell_in < 0 and item.quality > 0:
+            item.quality -= 1
+
+
+class AgedBrieUpdater(ItemUpdater):
+    def update(self, item):
+        if item.quality < 50:
+            item.quality += 1
+
+        item.sell_in -= 1
+
+        if item.sell_in < 0 and item.quality < 50:
+            item.quality += 1
+
+
+class BackstageUpdater(ItemUpdater):
+    def update(self, item):
+        if item.quality < 50:
+            item.quality += 1
+
+            if item.sell_in <= 10 and item.quality < 50:
+                item.quality += 1
+
+            if item.sell_in <= 5 and item.quality < 50:
+                item.quality += 1
+
+        item.sell_in -= 1
+
+        if item.sell_in < 0:
+            item.quality = 0
+
+
+class SulfurasUpdater(ItemUpdater):
+    def update(self, item):
+        pass
+
+
+class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    def get_updater(self, item):
+        if item.name == AGED_BRIE:
+            return AgedBrieUpdater()
+        elif item.name == BACKSTAGE:
+            return BackstageUpdater()
+        elif item.name == SULFURAS:
+            return SulfurasUpdater()
+        else:
+            return NormalUpdater()
+
     def update_quality(self):
         for item in self.items:
-            self._update_item(item)
-
-    def _is_aged_brie(self, item):
-        return item.name == AGED_BRIE
-
-    def _is_backstage(self, item):
-        return item.name == BACKSTAGE
-
-    def _is_sulfuras(self, item):
-        return item.name == SULFURAS
-
-    def _increase_quality(self, item):
-        if item.quality < 50:
-            item.quality = item.quality + 1
-
-    def _decrease_quality(self, item):
-        if item.quality > 0:
-            item.quality = item.quality - 1
-
-    def _set_quality_to_zero(self, item):
-        item.quality = 0
-
-    def _decrease_sell_in_if_needed(self, item):
-        if not self._is_sulfuras(item):
-            item.sell_in = item.sell_in - 1
-
-    # --- Type-specific updates (pure refactor: same behavior, clearer structure) ---
-
-    def _update_normal_item_before_sell_date(self, item):
-        if item.quality > 0:
-            if not self._is_sulfuras(item):
-                self._decrease_quality(item)
-
-    def _update_aged_brie_or_backstage_before_sell_date(self, item):
-        if item.quality < 50:
-            self._increase_quality(item)
-            if self._is_backstage(item):
-                if item.sell_in < 11:
-                    if item.quality < 50:
-                        self._increase_quality(item)
-                if item.sell_in < 6:
-                    if item.quality < 50:
-                        self._increase_quality(item)
-
-    def _update_after_sell_date(self, item):
-        if not self._is_aged_brie(item):
-            if not self._is_backstage(item):
-                if item.quality > 0:
-                    if not self._is_sulfuras(item):
-                        self._decrease_quality(item)
-            else:
-                self._set_quality_to_zero(item)
-        else:
-            if item.quality < 50:
-                self._increase_quality(item)
-
-    def _update_item(self, item):
-        # before sell date behavior
-        if (not self._is_aged_brie(item)) and (not self._is_backstage(item)):
-            self._update_normal_item_before_sell_date(item)
-        else:
-            self._update_aged_brie_or_backstage_before_sell_date(item)
-
-        # decrement sell_in (except sulfuras)
-        self._decrease_sell_in_if_needed(item)
-
-        # after sell date behavior
-        if item.sell_in < 0:
-            self._update_after_sell_date(item)
+            updater = self.get_updater(item)
+            updater.update(item)
--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -1,16 +1,29 @@
-# -*- coding: utf-8 -*-
 import unittest
-
 from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
 
-        
+    def test_aged_brie_increases_quality(self):
+        items = [Item("Aged Brie", 5, 10)]
+        GildedRose(items).update_quality()
+        self.assertEqual(11, items[0].quality)
+
+    def test_aged_brie_increases_twice_after_expiry(self):
+        items = [Item("Aged Brie", 0, 10)]
+        GildedRose(items).update_quality()
+        self.assertEqual(12, items[0].quality)
+
+    def test_backstage_pass_increase_by_two(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 10, 10)]
+        GildedRose(items).update_quality()
+        self.assertEqual(12, items[0].quality)
+
+    def test_backstage_pass_quality_drops_to_zero(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)]
+        GildedRose(items).update_quality()
+        self.assertEqual(0, items[0].quality)
+
+
 if __name__ == '__main__':
     unittest.main()
